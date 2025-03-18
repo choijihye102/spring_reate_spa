430debf2859549e021dcb95264860981c290b91c
ChoiJiHye <stdy1227@gmail.com>
2025-03-18 10:11:59 +0900

    Initial commit

---
+++ SemiprojectV2/src/main/java/com/example/jennie/semiprojectv2/SemiprojectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.jennie.semiprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiprojectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiprojectV2Application.class, args);
+	}
+
+}
---
+++ SemiprojectV2/src/test/java/com/example/jennie/semiprojectv2/SemiprojectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jennie.semiprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiprojectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
