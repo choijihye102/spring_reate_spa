477280bb50fd36786a24e8434dea22e116659b94
ChoiJiHye <stdy1227@gmail.com>
2025-03-18 11:29:45 +0900

    Main 출력

---
+++ semiprojectv2r/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// main 함수 컴포넌트 정의.
+const Main = () => {
+        return (
+            <main id="content">
+                <img src="image/profile.jpg" className="img-fluid"/>
+            </main>
+        )
+}
+
+export default Main;
--- semiprojectv2r/src/pages/layout/Header.jsx
+++ semiprojectv2r/src/pages/layout/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Nav = () => {
         return (
             <header>
-            <h1> SemiProject V1</h1>
+            <h1> SemiProject V2</h1>
             </header>
         )
     }
